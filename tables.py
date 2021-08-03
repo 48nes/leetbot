c14f11bc9eb01c833663c44b5ae0ea1a1efe1feb
Agnes <77949827+48nes@users.noreply.github.com>
2021-08-03 13:07:07 -0700

    database handler

---
+++ tables.py
@@ -0,0 +1,94 @@
+import os
+import psycopg2 as sql
+from psycopg2 import OperationalError
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = sql.connect(DATABASE_URL, sslmode='require')
+
+
+def connect(db_name, user_, password_, host_ip, port_):
+    connection = None
+    try:
+        connection = sql.connect(
+            database=db_name,
+            user=user_,
+            password=password_,
+            host=host_ip,
+            port=port_
+        )
+        print("Connection successful.")
+    except OperationalError as e:
+        print(f"Error '{e}' has occurred.")
+    return connection
+
+
+def query(connection, query_db):
+    connection.autocommit = True
+
+    try:
+        connection.cursor().execute(query_db)
+        print("Query executed.")
+    except OperationalError as e:
+        print(f"Error '{e}' has occurred.")
+
+
+def create_tables():
+    sql_commands = (
+        """
+        CREATE TABLE IF NOT EXISTS users (
+            discord_id INTEGER PRIMARY KEY,
+            leetcode_username TEXT NOT NULL,
+            most_recent TEXT,
+            num_problems INTEGER ARRAY[3]
+        )
+        """)
+    sql_conn = None
+    try:
+        # connect to the PostgreSQL server
+        sql_conn = sql.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        # create table one by one
+        for command in sql_commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        sql_conn.commit()
+    except (Exception, sql.DatabaseError) as error:
+        print(error)
+    finally:
+        if sql_conn is not None:
+            sql_conn.close()
+
+
+def check_discord(discord_id):
+    con = sql.connect(DATABASE_URL, sslmode='require')
+    cur = con.cursor()
+    cur.execute("SELECT leetcode_username FROM users WHERE discord_id =?", (discord_id,))
+    leetcode = cur.fetchone()
+    con.close()
+    if leetcode is None:
+        return ""
+    else:
+        return leetcode
+
+
+def check_leetcode(leetcode_username):
+    con = sql.connect(DATABASE_URL, sslmode='require')
+    cur = con.cursor()
+    cur.execute("SELECT 1 FROM users WHERE leetcode_username =?", (leetcode_username,))
+    num = cur.fetchone()
+    con.close()
+    return num == 1
+
+
+def insert_into_table(discord_id, leetcode_username, most_recent, num_easy, num_medium, num_hard):
+    con = sql.connect(DATABASE_URL, sslmode='require')
+    cur = con.cursor()
+    num_problems = [num_easy, num_medium, num_hard]
+    cur.execute(
+        "INSERT INTO users (discord_id,leetcode_username,most_recent,num_problems) VALUES (?,?,?,?)",
+        (discord_id, leetcode_username, most_recent, num_problems))
+    con.commit()
+    con.close()
